39f2b07a25104f2368acd931893e16f95a5e4069
Arian <arian.kulmer@web.de>
2019-10-04 13:44:00 +0100

    amami: Shipping SuperSU is deprecated
    * https://github.com/omnirom/android_bootable_recovery/commit/fa67cbf18bc43ff8cda34b91acdb43feb1214777
    
    Change-Id: I0f3e432368e92b12aa6411a30d17994a26e8ea44

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,6 @@ RECOVERY_SDCARD_ON_DATA := true
 # TWRP specific build flags
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/wled:backlight/brightness"
-TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_CRYPTO := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 255
